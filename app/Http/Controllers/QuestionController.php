9549381f6328247925390f583fde2ddca4bc69ec
Volodymyr Gavrylov <vova.intertech@gmail.com>
2015-08-03 18:55:56 +0300

    Added QuestionController

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Question;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class QuestionController extends Controller
+{
+    /**
+     * Show a list of all questions.
+     *
+     * @param Request $request
+     * @return \Illuminate\View\View
+     */
+    public function index(Request $request)
+    {
+        $order = $request->query->get('order', 'created_at');
+
+        $questions = Question::orderBy($order, 'desc')->get();
+
+        return view('question.index', ['questions' => $questions]);
+    }
+}
