e28515001915e3a7660bd4045f99c749a2cc57d0
Volodymyr Gavrylov <vova.intertech@gmail.com>
2015-08-03 18:57:55 +0300

    Changed route to homepage

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'QuestionController@index');
 
 // Authentication routes...
 Route::get('auth/login', 'Auth\AuthController@getLogin');
