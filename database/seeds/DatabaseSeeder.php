a492367245f18b4a9b92c2f950bde23b9564e6bf
Volodymyr Gavrylov <vova.intertech@gmail.com>
2015-08-03 18:55:41 +0300

    Added test data to database

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,5 +17,30 @@ class DatabaseSeeder extends Seeder
         // $this->call(UserTableSeeder::class);
 
         Model::reguard();
+
+        \App\User::create([
+            'name' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+
+        \App\User::create([
+            'name' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+
+        \App\Question::create([
+            'title' => str_random(10),
+            'description' => str_random(10),
+            'active' => 1,
+            'user_id' => 1,
+        ]);
+
+        \App\Answer::create([
+            'description' => str_random(10),
+            'user_id' => 2,
+            'question_id' => 1,
+        ]);
     }
 }
