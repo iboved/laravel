07485f1258122d61fc9dbe8e2b87d5ef4b7c8cb4
Volodymyr Gavrylov <vova.intertech@gmail.com>
2015-08-03 18:53:54 +0300

    Added Answer model

---
+++ app/Answer.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Answer extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['description', 'user_id', 'question_id'];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
+    /**
+     * Get the question that owns the answer.
+     */
+    public function question()
+    {
+        return $this->belongsTo('App\Question');
+    }
+
+    /**
+     * Get the user that owns the answer.
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+}
