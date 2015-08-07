7d8528a041f7f82a0cfce06bb538862f72105881
iboved <iboved@gmail.com>
2015-08-07 15:58:28 +0300

    Added valid active field to question table

---
+++ database/migrations/2015_08_06_110813_add_active_field_to_questions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddActiveFieldToQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->boolean('active')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->dropColumn('active');
+        });
+    }
+}
