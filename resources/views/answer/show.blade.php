179af426b4c57209b43124b89d64889c541b63d4
iboved <iboved@gmail.com>
2015-08-11 12:44:22 +0300

    Added answer show.blade.php

---
+++ resources/views/answer/show.blade.php
@@ -0,0 +1,16 @@
+@foreach ($answers as $answer)
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <h5>Author</h5>
+            Name: {{ $answer->user->name }} | Email: <a href="mailto:#"> {{ $answer->user->email }} </a>
+            <br>
+            Answer: {{ $answer->description }}
+            <p>Created at: {{ $answer->created_at }}</p>
+            @if (Auth::user() == $answer->user)
+                <div align="right">
+                    <a href="{{ action('AnswerController@delete', ['slug' => $question->slug, 'id' => $answer->id]) }}">Delete</a>
+                </div>
+            @endif
+        </div>
+    </div>
+@endforeach
