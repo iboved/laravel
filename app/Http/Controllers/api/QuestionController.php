4d59eda596c7078233a17efaefec4a34a66ebdda
iboved <iboved@gmail.com>
2015-09-08 11:29:33 +0300

    Added new methods in QuestionController

--- app/Http/Controllers/api/QuestionController.php
+++ app/Http/Controllers/api/QuestionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api;
 use Validator;
 use Illuminate\Http\Request;
 use App\Question;
+use App\Answer;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
@@ -75,29 +76,109 @@ class QuestionController extends Controller
     {
         $question = Question::findBySlugOrFail($slug);
 
-        $this->validate($request, [
+        $validator = Validator::make($request->all(), [
             'title' => 'required|max:150',
             'description' => 'required',
         ]);
 
-        $question->update([
-            'title' => $request->title,
-            'description' => $request->description,
-            'active' => $request->active
-        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        } else {
+            $question->update([
+                'title' => $request->title,
+                'description' => $request->description,
+                'active' => $request->active
+            ]);
 
-        return response()->json($question);
+            return response()->json($question);
+        }
     }
 
     /**
+     * Remove the specified resource from storage.
+     *
      * @param $slug
      * @return \Illuminate\Http\JsonResponse
      */
     public function destroy($slug)
     {
         $question = Question::findBySlug($slug);
+
         if ($question) {
             $question->delete();
+        }
+
+        return response()->json([], 204);
+    }
+
+    /**
+     * Display a listing of the related resource.
+     *
+     * @param $slug
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getAnswers($slug)
+    {
+        $question = Question::findBySlugOrFail($slug);
+
+        $answers = $question->answers;
+
+        return response()->json($answers);
+    }
+
+    /**
+     * Store a newly created related resource in storage.
+     *
+     * @param Request $request
+     * @param $slug
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function storeAnswer(Request $request, $slug)
+    {
+        $question = Question::findBySlugOrFail($slug);
+
+        if ($question->active) {
+            $validator = Validator::make($request->all(), [
+                'description' => 'required',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 400);
+            } else {
+                $answer = new Answer([
+                    'description' => $request->description,
+                    'user_id' => Auth::user()->id,
+                    'question_id' => $question->id
+                ]);
+                $answer->save();
+
+                return response()->json($answer, 201);
+            }
+        } else {
+            return response()->json('Forbidden', 403);
+        }
+    }
+
+    /**
+     * Remove the specified related resource from storage.
+     *
+     * @param $slug
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function deleteAnswer($slug, $id)
+    {
+        $question = Question::findBySlugOrFail($slug);
+        $answer = $question->answers->find($id);
+
+//        $answer = Answer::find($id);
+
+        if ($answer) {
+            if (Auth::user() == $answer->user) {
+                $answer->delete();
+            } else {
+                return response()->json('Forbidden', 403);
+            }
         }
 
         return response()->json([], 204);
