7cddcc659c6acdea816f9baee8f136bae20f0719
Vova Gavrylov <iboved@gmail.com>
2015-09-11 17:45:57 +0300

    Fixed likes AJAX request

--- public/js/likes.js
+++ public/js/likes.js
@@ -1,21 +1,26 @@
 $(document).ready(function() {
-    $('#like').click(function() {
+    $('#like').on('click', function(e) {
+        e.preventDefault();
 
+        $.ajax({
+            type: "POST",
+            url: $(this).attr("href"),
+            context: $('#like'),
+            success: function(data) {
+                //console.log(data);
 
-        if ($(this).hasClass('btn btn-primary')) {
-            $(this).removeClass('btn btn-primary');
-            $(this).addClass('btn btn-danger');
-            $(this).html('Dislike');
-        } else {
-            $(this).removeClass('btn btn-danger');
-            $(this).addClass('btn btn-primary');
-            $(this).html('Like');
-        }
+                $('#likes-count').text('Likes: ' + data);
+
+                if ($(this).hasClass('btn btn-primary')) {
+                    $(this).removeClass('btn btn-primary');
+                    $(this).addClass('btn btn-danger');
+                    $(this).text('Dislike');
+                } else {
+                    $(this).removeClass('btn btn-danger');
+                    $(this).addClass('btn btn-primary');
+                    $(this).text('Like');
+                }
+            }
+        });
     });
 });
-
-
-//$.ajax({
-//    type: "GET",
-//    url: "/questions/{slug}/like"
-//});
